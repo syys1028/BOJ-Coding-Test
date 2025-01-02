b28dad491a65d5e74bc4674b0916e6a95787c7e6
서연 <107555293+syys1028@users.noreply.github.com>
2025-01-02 17:59:40 +0900

    Update A＋B － 8.py

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -2,5 +2,5 @@ t = int(input());
 
 for i in range(1, t+1):
     a, b = input().split();
-    sum = int(a) + int(b)
+    sum = int(a) + int(b);
     print("Case #{}: {} + {} = {}".format(i,a,b,sum));
