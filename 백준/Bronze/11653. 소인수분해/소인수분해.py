9b1be91ceada46d28d1ec52261a3c419082ef931
서연 <107555293+syys1028@users.noreply.github.com>
2025-01-05 23:54:01 +0900

    [Bronze I] Title: 소인수분해, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,11 +1,11 @@
 n = int(input())
 a = 2
 
-while a <= (n // 2 + 1):
+while a * a <= n:
     if n % a == 0:
         n = n // a
         print(a)
     else:
         a += 1
-    if a > (n // 2 + 1) and n != 1:
-        print(n)
+if n > 1:
+    print(n)
