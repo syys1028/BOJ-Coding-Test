be973be667340368ecfa913a013d51df31278808
서연 <107555293+syys1028@users.noreply.github.com>
2025-01-06 23:42:21 +0900

    [Silver V] Title: 수들의 합, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,10 +1,6 @@
 s = int(input())
-max_num = 0
-n = s
-while n > -1:
-    if (n - (max_num + 1)) >= 0:
-        max_num += 1
-        n = n - max_num
-    else:
-        print(max_num)
-        n = -1
+n = 1
+while n * (n + 1) // 2 <= s:
+    n += 1
+    
+print(n-1)
